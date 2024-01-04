7ab3118652e2abfafdcf8186f9a9fba98ee00f83
alanvarghese52 <alanvarghese2516@gmail.com>
2024-01-04 16:09:00 +0530

    Product details ui part - 2 done

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
+import 'package:zenshop/common/widgets/texts/section_heading.dart';
+import 'package:zenshop/features/shop/screens/product_details/widgets/bottom_add_to_cart.dart';
+import 'package:zenshop/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:zenshop/utils/constants/sizes.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/widgets/product_detail_image_slider.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/widgets/product_meta_data.dart';
@@ -9,33 +14,79 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const TBottomAddToCard(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// 1-product image slider
-            TProductImageSlider(),
+            const TProductImageSlider(),
 
             /// 2-product details
-            Padding(padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace,bottom: TSizes.defaultSpace),
-            child: Column(
-              children: [
-                /// -rating & share
-                TRatingAndShare(),
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// -rating & share
+                  const TRatingAndShare(),
 
-                /// -price, title,stack & brand
-                TProductMetaData(),
+                  /// -price, title,stack & brand
+                  const TProductMetaData(),
+                  const SizedBox(height: TSizes.spaceBtwItems / 2),
 
-                /// -attributes
-                /// -description
-                /// -reviews
-              ],
-            ),)
+                  /// -attributes
+                  const ProductAttributes(),
+                  const SizedBox(height: TSizes.spaceBtwItems / 2),
+
+                  /// checkout button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Checkout'),
+                    ),
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+
+                  /// -description
+                  const TSectionHeading(
+                      title: 'Description', showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const ReadMoreText(
+                    'This is a product description for blue nike t shirt. there are more things that can be  there are more things that can be added but i am writing now.',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show more',
+                    trimExpandedText: 'Less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
+                  /// -reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                          title: 'Review(199)', showActionButton: false),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Iconsax.arrow_right_3))
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
